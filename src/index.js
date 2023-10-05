e6f668611dcd9d4db0fb35c69641902f4a96acd0
StevieG46 <126635670+StevieG46@users.noreply.github.com>
2023-10-05 20:29:53 +1100

    routing established with dummy component

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ApiProvider from './contexts/ApiContext';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CardSearchByName from './pages/CardSearchByName';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ApiProvider>
+
+      <BrowserRouter>
+      <Routes>
+        {/*  Homepage Route */}
+        <Route path="/" element={<App />} />
+        {/* API fetch Route */}
+        <Route path="/card/search/:pokemonName" element={<CardSearchByName />} /> 
+
+        
+      </Routes>
+      
+      </BrowserRouter>
       <App />
     </ApiProvider>
   </React.StrictMode>
