01111bf113eeb25af9a0200483ff2c3a8dc1b47c
Stephen Gow <126635670+StevieG46@users.noreply.github.com>
2023-10-07 10:36:15 +1100

    feat: fetch data from api and display it (#14)
    * routing established with dummy component
    
    * progress on api request
    
    * progress api key
    
    * feat: card search working
    
    * react bootstrap installed card array set

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,22 @@
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function PokemonCard(props) {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={props.imageUrl} />
+      <Card.Body>
+        <Card.Title>{props.cardTitle}</Card.Title>
+        <Card.Text>
+          {props.cardDescritpion}
+        </Card.Text>
+        <Button variant="primary">
+            View Card
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default PokemonCard;
