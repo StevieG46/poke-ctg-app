d09d18e33d5cb92edabb16ec98fc6df1fa9875f1
StevieG46 <126635670+StevieG46@users.noreply.github.com>
2023-10-07 12:19:22 +1100

    bootstrap setup properly configured

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,20 +3,22 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 function PokemonCard(props) {
-  return (
-    <Card style={{ width: '18rem'}}>
-        <Card.Img variant="top" src={props.imageUrl} style={{maxHeight: 342, maxWidth: 245}} />
-      <Card.Body>
-        <Card.Title>{props.cardTitle}</Card.Title>
-        <Card.Text>
-          {props.cardDescription}
-        </Card.Text>
-        <Button variant="primary">
-            View Card
-        </Button>
-      </Card.Body>
-    </Card>
-  );
-}
+    return (
+        <Card style={{ width: '18rem'}}>
+          <Card.Img variant="top" src={props.imageUrl} style={{maxHeight: 342, maxWidth: 245}} />
+          <Card.Body>
+            <Card.Title>
+                {props.cardTitle}
+            </Card.Title>
+            <Card.Text>
+              {props.cardDescription}
+            </Card.Text>
+            <Button variant="primary">
+                View Card
+            </Button>
+          </Card.Body>
+        </Card>
+      );
+    }
 
 export default PokemonCard;
